989697e0a328b9bd3a216565c4627be38bebce69
Serelik <zdivanal@gmail.com>
2025-05-21 17:55:50 +0500

    Booklist like animation

--- app/src/main/java/com/serelik/surfbooks/ui/common/BookItemUi.kt
+++ app/src/main/java/com/serelik/surfbooks/ui/common/BookItemUi.kt
@@ -1,25 +1,37 @@
 package com.serelik.surfbooks.ui.common
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.serelik.surfbooks.R
 import com.serelik.surfbooks.ui.favorite.BookItemUiModel
@@ -55,23 +67,48 @@ fun BookItemUi(
                     )
                 }
 
-                val color = if (bookItemUiModel.isFavorite) Color.Red else Color.LightGray
-                Icon(
-                    imageVector = Icons.Filled.Favorite,
-                    tint = color,
-                    contentDescription = stringResource(R.string.favorite_description),
-                    modifier = Modifier
+                val size: Dp by animateDpAsState(
+                    if (bookItemUiModel.isFavorite) 24.dp else 0.dp
+                )
+
+                Box(
+                    Modifier
                         .align(Alignment.TopEnd)
-                        .padding(top = 6.dp, end = 10.dp)
-                        .background(color = Color.White, shape = ShapeDefaults.ExtraLarge)
-                        .padding(6.dp)
-                        .clickable {
+                        .clickable(
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        ) {
+
                             onFavoriteClickSnackBar(bookItemUiModel.isFavorite)
                             onFavoriteClick(bookItemUiModel)
-
-
                         }
-                )
+
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        tint = Color.LightGray,
+                        contentDescription = stringResource(R.string.favorite_description),
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(top = 6.dp, end = 10.dp)
+                            .background(color = Color.White, shape = ShapeDefaults.ExtraLarge)
+                            .padding(6.dp)
+
+                    )
+
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        tint = Color.Red,
+                        contentDescription = stringResource(R.string.favorite_description),
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(top = 6.dp, end = 10.dp)
+                            .padding(6.dp)
+                            .size(size)
+                    )
+
+                }
             }
         }
 
